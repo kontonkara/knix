f11ae491c8572be3733581d02f4c78ffce7571ce
Nikita Konton <kontonkara@gmail.com>
2025-04-06 02:39:52 +0300

    user: change shell to fish, ignore program check

--- system/user.nix
+++ system/user.nix
@@ -2,6 +2,8 @@
 
 {
   users.users.${username} = {
+    shell = pkgs.fish;
+    ignoreShellProgramCheck = true;
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
     hashedPassword = "$6$9H9XYvFnK3rZOZCb$5bf3tFa9wpLO.Ad.XJwtbeIRROH0bp6Uy6wh/ZnZsLmdJLmiD.YBJPNBEA8y7R9Wi8jmow/uax6luAXCcdfTm/";
