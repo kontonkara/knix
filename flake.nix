25de8a0ac6516a10cd4652e072c04b8c94b11d21
Nikita Konton <kontonkara@gmail.com>
2025-04-06 03:13:07 +0300

    flake: fix devshell

--- flake.nix
+++ flake.nix
@@ -17,6 +17,21 @@
       pkgs = nixpkgs.legacyPackages.${system};
       username = "kontonkara";
       networkname = "hp";
+      findModules = dir:
+        builtins.concatLists (builtins.attrValues (builtins.mapAttrs
+          (name: type:
+            if type == "regular" then
+              [{
+                name = builtins.elemAt (builtins.match "(.*)\\.nix" name) 0;
+                value = dir + "/${name}";
+              }]
+            else if (builtins.readDir (dir + "/${name}"))
+              ? "default.nix" then [{
+              inherit name;
+              value = dir + "/${name}";
+            }] else
+              findModules (dir + "/${name}"))
+          (builtins.readDir dir)));
     in
     {
       nixosConfigurations.${networkname} = nixpkgs.lib.nixosSystem {
